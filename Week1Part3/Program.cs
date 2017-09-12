9c5e923dd0ba3b137cf62c2a98782690ebe0c1e2
WillRichey1234 <gsampson@vms.edu>
2017-09-12 15:59:48 -0600

    Week1Part3

---
+++ Week1Part3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeworkWeek1DeclaringVariablesPart3
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int x = 7;
+			string y = "5";
+			string myFirstTry = x.ToString();
+
+			// this try did not work becasue we did
+			// not convert the data yet and it still 
+			//didnt because bob did is able to be converted.
+			//int mySecondTry = x + y;
+			int mySecondTry = x + int.Parse(y);
+			Console.WriteLine(mySecondTry);
+
+			Console.ReadLine();
+
+		}
+	}
+}
